edf57f7e8b48378fa4eefbabf7d7fbae985a9b08
story <lsy1538756437@126.com>
2024-10-19 20:49:13 -0700

    RegisterIP

--- frontend/src/context/dynamic.tsx
+++ frontend/src/context/dynamic.tsx
@@ -11,6 +11,74 @@ import { FlowWalletConnectors } from "@dynamic-labs/flow"
 interface AppProps {
     children: React.ReactNode
 }
+const evmNetworks = [
+    {
+      blockExplorerUrls: ['https://etherscan.io/'],
+      chainId: 1,
+      chainName: 'Ethereum Mainnet',
+      iconUrls: ['https://app.dynamic.xyz/assets/networks/eth.svg'],
+      name: 'Ethereum',
+      nativeCurrency: {
+        decimals: 18,
+        name: 'Ether',
+        symbol: 'ETH',
+        iconUrl: 'https://app.dynamic.xyz/assets/networks/eth.svg',
+      },
+      networkId: 1,
+  
+      rpcUrls: ['https://mainnet.infura.io/v3/'],
+      vanityName: 'ETH Mainnet',
+    },
+  {
+      blockExplorerUrls: ['https://etherscan.io/'],
+      chainId: 5,
+      chainName: 'Ethereum Goerli',
+      iconUrls: ['https://app.dynamic.xyz/assets/networks/eth.svg'],
+      name: 'Ethereum',
+      nativeCurrency: {
+        decimals: 18,
+        name: 'Ether',
+        symbol: 'ETH',
+        iconUrl: 'https://app.dynamic.xyz/assets/networks/eth.svg',
+      },
+      networkId: 5,
+      rpcUrls: ['https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'],
+  
+      vanityName: 'Goerli',
+    },
+    {
+      blockExplorerUrls: ['https://polygonscan.com/'],
+      chainId: 137,
+      chainName: 'Matic Mainnet',
+      iconUrls: ["https://app.dynamic.xyz/assets/networks/polygon.svg"],
+      name: 'Polygon',
+      nativeCurrency: {
+        decimals: 18,
+        name: 'MATIC',
+        symbol: 'MATIC',
+        iconUrl: 'https://app.dynamic.xyz/assets/networks/polygon.svg',
+      },
+      networkId: 137,
+      rpcUrls: ['https://polygon-rpc.com'],
+      vanityName: 'Polygon',
+    },
+    {
+        blockExplorerUrls: ['https://testnet.storyscan.xyz/'],
+        chainId: 1513,
+        chainName: 'Story Public Testnet',
+        iconUrls: ["https://images.ctfassets.net/5ei3wx54t1dp/5ckeqBCMdZeazgUHzbHXPt/1021e9047c960b9d1f3b9b74726d877c/Story_Protocol_Icon.svg"],
+        name: 'Story',
+        nativeCurrency: {
+          decimals: 18,
+          name: 'IP',
+          symbol: 'IP',
+          iconUrl: 'https://images.ctfassets.net/5ei3wx54t1dp/5ckeqBCMdZeazgUHzbHXPt/1021e9047c960b9d1f3b9b74726d877c/Story_Protocol_Icon.svg',
+        },
+        networkId: 1513,
+        rpcUrls: ['https://testnet.storyrpc.io/'],
+        vanityName: 'Story Public Testnet',
+      },
+  ];
 
 const DynamicProvider: React.FC<AppProps> = ({ children }) => {
     const { logOut, fetchUser, setUserLogging } = useUser()
@@ -89,6 +157,7 @@ const DynamicProvider: React.FC<AppProps> = ({ children }) => {
                         logOut()
                     },
                 },
+                overrides: { evmNetworks },
             }}
         >
             {children}
