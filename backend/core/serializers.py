24a386dcaca4751e996f57ead67f3bc1f48f23ed
Henry Yuan <henry.yuan@nyu.edu>
2024-10-20 18:43:24 +0800

    update

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -3,14 +3,18 @@ from .models import CodeBlock
 class CodeBlockSerializer(serializers.ModelSerializer):
     children = serializers.SerializerMethodField()
     parent = serializers.CharField(required=False, allow_null=True)
+    parent_token_id = serializers.CharField(required=False, allow_null=True)
 
     class Meta:
         model = CodeBlock
-        fields = ['id', 'name', 'wallet_address', 'parent', 'code', 'token_id', 'created_at', 'updated_at', 'children']
-        read_only_fields = ['created_at', 'updated_at', 'children']
+        fields = ['id', 'name', 'wallet_address', 'parent', 'code', 'token_id', 'created_at', 'updated_at', 'children', 'parent_token_id']
+        read_only_fields = ['created_at', 'updated_at', 'children', 'parent_token_id']
 
     def get_children(self, obj):
         return [{'id': child.id, 'token_id': child.token_id} for child in obj.children.all()]
+    
+    def get_parent_token_id(self, obj):
+        return obj.parent.token_id if obj.parent else ""
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
